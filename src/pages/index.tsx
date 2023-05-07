964a23e603797daa43c8e6edcbdac43b37ec3882
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2023-05-07 14:08:32 +0200

    sites update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,15 +43,13 @@ function Home({categories, order}: HomePageProps) {
 
 export default Home
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-    // const res = await axios.get(`${API_URL}/api/products`)
-    //     .then((res) => {
-    //         return res.data
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     })
-    // const categories = [...new Set<string>(res.map((product: IProduct) => product.category))]
-
-    const categories = ['headphones', 'speakers', 'earphones']
+    const res = await axios.get(`${API_URL}/api/products`)
+        .then((res) => {
+            return res.data
+        }).catch((err) => {
+            console.log(err)
+        })
+    const categories = [...new Set<string>(res.map((product: IProduct) => product.category))]
 
     let order: IOrder[] | null = null;
     if (context.req.url?.includes("orderId")) {
