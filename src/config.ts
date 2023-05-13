eca2d4a269dd0e6408785dc8686f4cb9eaee652c
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2023-05-13 16:03:03 +0200

    API url changed

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-// export const API_URL = "https://audiophile-ecommerce-sigma.vercel.app";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://audiophile-ecommerce-sigma.vercel.app";
+// export const API_URL = "http://localhost:3000";
 
