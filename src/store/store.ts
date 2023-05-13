bf280ba7bff3651a6f935ff65db24347c1a37a2d
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2023-05-13 15:56:44 +0200

    shopping cart in localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,29 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import cartReducer from '../features/cart/cartSlice'
 import addressReducer from '../features/address/addressSlice'
+import {persistReducer, persistStore} from 'redux-persist'
+import thunk from 'redux-thunk'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    address: addressReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        address: addressReducer,
-    },
+    reducer: persistedReducer,
+    middleware: [thunk],
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
